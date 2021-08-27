e30a66c99c3dceab843d90aac77109f20f2ff2da
Sfrev <51010839+Sfrev@users.noreply.github.com>
2021-08-27 18:41:54 -0300

    Initial commit

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [Header("Background Scroller")]
+    [SerializeField] float backgroundScrollSpeed = 0.1f;
+
+    Material myMaterial;
+    Vector2 offset;
+
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(backgroundScrollSpeed, 0f);
+    }
+
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject spherePrefab;
+    [Tooltip("Time in seconds")] [SerializeField] private float spawnTime;
+    [SerializeField] private Vector3 spawnPosition;
+    [SerializeField] private bool spawn = false;
+    [SerializeField] private float forceStrengh = 20f;
+    private float timer = 0f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnTime && spawn)
+        {
+            timer = 0f;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject sphere = Instantiate(spherePrefab, spawnPosition, Quaternion.identity);
+
+        Vector3 force = new Vector3(Random.Range(-forceStrengh, forceStrengh), 
+            Random.Range(-forceStrengh, forceStrengh), Random.Range(-forceStrengh, forceStrengh));
+
+        sphere.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+
+        Destroy(sphere, 5f);
+    }
+}
